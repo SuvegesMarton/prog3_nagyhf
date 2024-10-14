841e1c3cc4087dbb764a85d364ac0a9e8fd0fa16
SuvegesMarton <suvegesmarton@gmail.com>
2024-10-14 15:01:04 +0200

    added a constructor and getters to the grid class

--- src/grid/Grid.java
+++ src/grid/Grid.java
@@ -1,6 +1,32 @@
 package grid;
 
 public class Grid {
+	private final int gridSizeBase;
+	private int[] values;
+	private boolean[] isHardcoded;
+
+	public Grid(int gridSizeBase) {
+		this.gridSizeBase = gridSizeBase;
+		int numberOfFields = (int)Math.pow(gridSizeBase, 4);
+		this.values = new int[numberOfFields];
+		this.isHardcoded = new boolean[numberOfFields];
+	}
+
+	private int xyToScalar(int x, int y) {
+		return y*this.gridSizeBase*this.gridSizeBase + x;
+	}
+
+	public int getGridSizeBase() {
+		return this.gridSizeBase;
+	}
+
+	public int getValue(int x, int y) {
+		return this.values[xyToScalar(x, y)];
+	}
+	
+	public boolean getIsHardCoded(int x, int y) {
+		return this.isHardcoded[xyToScalar(x, y)];
+	}
     public static void print() {
 		System.out.println("Hello Grid!");
 	}
--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,7 @@ import grid.Grid;
 class Main {
     public static void main(String[] args) {
 		System.out.println("Hello World!");
-		Grid a = new Grid();
-		a.print();
+		Grid a = new Grid(3);
+		a.getValue(3, 4);
 	}
 }
